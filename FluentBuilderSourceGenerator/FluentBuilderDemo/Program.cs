69f1cc3e2066d8fabb61f991d5e76bcc4ecb9cd2
Karasini <Karasini@users.noreply.github.com>
2023-10-16 21:27:32 +0200

    Remove unused using namespace

--- FluentBuilderSourceGenerator/FluentBuilderDemo/Program.cs
+++ FluentBuilderSourceGenerator/FluentBuilderDemo/Program.cs
@@ -1,7 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-using FluentBuilderDemo;
-
 var user = new UserBuilder().WithFirstName("Gildia").WithLastName(".NET").Build();
 
 Console.WriteLine($"Hello, {user.FirstName} {user.LastName}");
